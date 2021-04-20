ca7e35c2d490a224c7d6ad5345746d148c363ec7
leonyangela <38250310+leonyangela@users.noreply.github.com>
2021-04-20 22:28:58 +0700

    add container pattern, bugfix, and add the shopPage to the firebase database

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
+import { compose } from 'redux'
+import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors'
+import WithSpinner from '../with-spinner/with-spinner.component'
+import CollectionsOverviewComponent from './collections-overview.component'
+import CollectionsOverview from './collections-overview.component'
+
+const mapStateToProps = createStructuredSelector({
+  isLoading: selectIsCollectionFetching,
+})
+
+const CollectionsOverviewContainer = compose(
+    // Will evaluates withSpinner first then passing 
+    // collectionsOverview to withSpinner, then passing
+    // collectionsOverview to connect(mapStateToProps)
+  connect(mapStateToProps),
+  WithSpinner
+)(CollectionsOverview)
+
+export default CollectionsOverviewContainer
