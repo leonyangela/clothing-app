049771dd58a7cd1a459d28c78e5bcffdab189c80
leonyangela <38250310+leonyangela@users.noreply.github.com>
2021-07-11 22:22:09 +0700

    fix checkout button & add axios to the client

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   // publishable key can be found on stripe dashboard
   const publishableKey = 'pk_test_51IRJDWEVNCKy6KQhqqny9Z6MX08wTymmRuLrj6e8A95ZVp5bugkFtTjNnJrrL8Rm8WQ0Dtpo9NL7ok6BhYqxK41800UyOljzlD';
-  const onToken = (token) => {
-    console.log(token);
-    alert('Payment Successful');
+  // const onToken = (token) => {
+  //   console.log(token);
+  //   alert('Payment Successful');
+  // };
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    })
+      .then(response => {
+        alert('succesful payment');
+      })
+      .catch(error => {
+        console.log('Payment Error: ', error);
+        alert(
+          'There was an issue with your payment! Please make sure you use the provided credit card.'
+        );
+      });
   };
+
 
   return (
     <StripeCheckout
