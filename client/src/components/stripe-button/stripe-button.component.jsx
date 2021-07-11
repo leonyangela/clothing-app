cfb99934d4477aad1d8c731d752bf2589ade3fe9
leonyangela <38250310+leonyangela@users.noreply.github.com>
2021-07-11 21:36:07 +0700

    change public key

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_51IRJDWEVNCKy6KQhSYLLKAcjbnLLSl45iNR1YG7Ao760y4Ize4HnPzY0iZVziiC6H1vnm1HZAdokD8NCz1RFRZ6Z00TH6n7CRO';
+  const publishableKey = process.env.REACT_APP_STRIPE_KEY;
   const onToken = (token) => {
     console.log(token);
     alert('Payment Successful');
